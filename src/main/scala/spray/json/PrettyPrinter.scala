spray/spray-json

commit 3f56c8f4c6e7ede3d996b8e64554030bfc401c54

Make printer methods protected, so they can be overridden

src/main/scala/spray/json/PrettyPrinter.scala
@@ -29,7 +29,7 @@ trait PrettyPrinter extends JsonPrinter {
     print(x, sb, 0)
   }
   
-  private def print(x: JsValue, sb: StringBuilder, indent: Int) {
+  protected def print(x: JsValue, sb: StringBuilder, indent: Int) {
     x match {
       case JsObject(x) => printObject(x, sb, indent)
       case JsArray(x)  => printArray(x, sb, indent)
@@ -37,7 +37,7 @@ trait PrettyPrinter extends JsonPrinter {
     }
   }
 
-  private def printObject(members: Map[String, JsValue], sb: StringBuilder, indent: Int) {
+  protected def printObject(members: Map[String, JsValue], sb: StringBuilder, indent: Int) {
     sb.append("{\n")    
     printSeq(members, sb.append(",\n")) { m =>
       printIndent(sb, indent + Indent)
@@ -50,14 +50,14 @@ trait PrettyPrinter extends JsonPrinter {
     sb.append("}")
   }
   
-  private def printArray(elements: List[JsValue], sb: StringBuilder, indent: Int) {
+  protected def printArray(elements: List[JsValue], sb: StringBuilder, indent: Int) {
     sb.append('[')
     printSeq(elements, sb.append(", "))(print(_, sb, indent))
     sb.append(']')
   }
   
   @tailrec
-  private def printIndent(sb: StringBuilder, indent: Int) {
+  protected def printIndent(sb: StringBuilder, indent: Int) {
     if (indent > 0) {
       sb.append(' ')
       printIndent(sb, indent - 1)

src/main/scala/spray/json/CompactPrinter.scala
@@ -31,7 +31,7 @@ trait CompactPrinter extends JsonPrinter {
     }
   }
 
-  private def printObject(members: Map[String, JsValue], sb: StringBuilder) {
+  protected def printObject(members: Map[String, JsValue], sb: StringBuilder) {
     sb.append('{')
     printSeq(members, sb.append(',')) { m =>
       printString(m._1, sb)
@@ -41,7 +41,7 @@ trait CompactPrinter extends JsonPrinter {
     sb.append('}')
   }
 
-  private def printArray(elements: List[JsValue], sb: StringBuilder) {
+  protected def printArray(elements: List[JsValue], sb: StringBuilder) {
     sb.append('[')
     printSeq(elements, sb.append(','))(print(_, sb))
     sb.append(']')

commit af38d4df5a099acaba859ea8364a573ac35b8dfe

Fix error introduced in 3f56c8f (#92)

src/main/scala/spray/json/PrettyPrinter.scala
@@ -56,12 +56,13 @@ trait PrettyPrinter extends JsonPrinter {
     sb.append(']')
   }
   
-  @tailrec
   protected def printIndent(sb: StringBuilder, indent: Int) {
-    if (indent > 0) {
-      sb.append(' ')
-      printIndent(sb, indent - 1)
-    }
+    @tailrec def rec(indent: Int): Unit =
+      if (indent > 0) {
+        sb.append(' ')
+        rec(indent - 1)
+      }
+    rec(indent)
   }
 }
 