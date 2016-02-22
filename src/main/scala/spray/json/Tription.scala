e71ac5f7b06c862f223d274eafc4c148032a5962
bathalh <bathalh@monsanto.com>
2016-02-22 11:56:00 -0600

    Adding Tription JavaDoc

--- src/main/scala/spray/json/Tription.scala
+++ src/main/scala/spray/json/Tription.scala
@@ -1,7 +1,22 @@
 package spray.json
 
 /**
-  * A Triple-Option for JSON values: `Undefined`, `Null`, or `Value(x)` See readme for more details
+  * A Triple-Option
+  *
+  * JavaScript (and JSON), unlike Java/Scala, allow undefined values, which are distinct from null values.
+  * For example, a PUT request may have a payload like this:
+  * <code>
+  *     { "id":"234565434567898789098765",
+  *       "field1": 7,
+  *       "field3: null,
+  *       "field4": undefined }
+  * </code>
+  * which would tell the server to update field1 to 7, set field3 to null, and leave field2 and field4 alone.
+  * With a standard scala `Option`, it is impossible to tell whether the payload of the request had field2, field3,
+  * and field4 null or undefined since any missing values translate to `None`.
+  *
+  * The Tription solves that problem by defining `Value` for present values, `Null` for null values, and
+  * `Undefined` for values which are missing or explicitly marked as undefined.
   *
   * Created by bathalh on 2/19/16.
   */
