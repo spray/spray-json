af96271b02f54666364d689a51b0428d44450f38
Jiro Kugiya <j.kugiya@gmail.com>
2019-04-21 17:54:27 +0900

    Applyied [agemooij's implementations](https://gist.github.com/agemooij/767913://gist.github.com/agemooij/7679130)

--- src/main/scala/spray/json/SnakeCaseJsonSupport.scala
+++ src/main/scala/spray/json/SnakeCaseJsonSupport.scala
@@ -1,52 +1,51 @@
+/**
+  * The MIT License (MIT)
+  *
+  * Copyright (c) 2013-2015 Andrew Snare, Age Mooij
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
+  */
 package spray.json
 
-import scala.reflect.ClassTag
-import scala.collection.mutable
-
 /**
-  * Provides snake cased JsonFormats
+  * A custom version of the Spray DefaultJsonProtocol with a modified field naming strategy
   */
 trait SnakeCaseJsonSupport extends DefaultJsonProtocol {
+  import reflect._
 
-  override protected def extractFieldNames(classTag: ClassTag[_]): Array[String] = {
-    def snakify(name: String) = {
-      val chars = name.toCharArray
-      val len = name.length
-      val sb = new mutable.StringBuilder(len + len / 5)
+  /**
+    * This is the most important piece of code in this object!
+    * It overrides the default naming scheme used by spray-json and replaces it with a scheme that turns camelcased
+    * names into snakified names (i.e. using underscores as word separators).
+    */
+  override protected def extractFieldNames(classTag: ClassTag[_]) = {
+    import java.util.Locale
 
-      def isAlphabetic(char: Char): Boolean = (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
+    def snakify(name: String) = PASS2.replaceAllIn(PASS1.replaceAllIn(name, REPLACEMENT), REPLACEMENT).toLowerCase(Locale.US)
 
-      def go(i: Int, rest: Int, processedUpper: Boolean, processedAlphaNumeric: Boolean): String =
-        if (rest == 0) {
-          sb.toString()
-        } else if (rest > 1 && chars(i).isUpper && chars(i + 1).isLower) {
-          if (processedAlphaNumeric) {
-            sb.append('_')
-          }
-          sb.append(chars(i).toLower).append(chars(i + 1))
-          go(i + 2, rest - 2, false, true)
-        } else if (chars(i).isUpper) {
-          if (!processedUpper && processedAlphaNumeric) {
-            sb.append('_')
-          }
-          sb.append(chars(i).toLower)
-          go(i + 1, rest - 1, true, chars(i).isDigit || isAlphabetic(chars(i)))
-        } else if (!isAlphabetic(chars(i))) {
-          sb.append(chars(i))
-          go(i + 1, rest - 1, false, chars(i).isDigit)
-        } else {
-          sb.append(chars(i).toLower)
-          go(i + 1, rest - 1, chars(i).isUpper, chars(i).isDigit || isAlphabetic(chars(i)))
-        }
-
-      go(0, len, true, false)
-    }
-
-    super.extractFieldNames(classTag).map {
-      snakify(_)
-    }
+    super.extractFieldNames(classTag).map { snakify(_) }
   }
 
+  private val PASS1 = """([A-Z]+)([A-Z][a-z])""".r
+  private val PASS2 = """([a-z\d])([A-Z])""".r
+  private val REPLACEMENT = "$1_$2"
 }
 
 object SnakeCaseJsonSupport extends SnakeCaseJsonSupport
+
