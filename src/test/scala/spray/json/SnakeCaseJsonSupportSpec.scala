56e699db803c62f2a14dc9cf304a3bbaa4fa5266
Jiro Kugiya <j.kugiya@gmail.com>
2019-04-21 17:35:30 +0900

    Fixed expectations

--- src/test/scala/spray/json/SnakeCaseJsonSupportSpec.scala
+++ src/test/scala/spray/json/SnakeCaseJsonSupportSpec.scala
@@ -1,19 +1,10 @@
 package spray.json
 
-import java.util.Locale
-
 import org.specs2.mutable._
 
 class SnakeCaseJsonSupportSpec extends Specification with SnakeCaseJsonSupport {
 
-  private val PASS1 = """([A-Z]+)([A-Z][a-z])""".r
-  private val PASS2 = """([a-z\d])([A-Z])""".r
-  private val REPLACEMENT = "$1_$2"
-
-  def snakify(name: String) =
-    PASS2.replaceAllIn(PASS1.replaceAllIn(name, REPLACEMENT), REPLACEMENT).toLowerCase(Locale.US)
-
-  "SnakeCaseJsonSUpport" should {
+  "SnakeCaseJsonSupport" should {
     "convert field names to snake cased fields names." in {
       // Given
       case class SampleFields(
@@ -26,18 +17,18 @@ class SnakeCaseJsonSupportSpec extends Specification with SnakeCaseJsonSupport {
           `tHere-Are_(AlsO)_noN_(alphaBetic))_CharaCter!s`: Int = 0,
           `  There are space character `: Int = 0,
           ` これは　マルチバイトAbcのaBc確認です。`: Int = 0,
-          aa: Int = 0,
-          AA: Int = 0,
-          Aa: Int = 0,
-          aA: Int = 0,
+          ab: Int = 0,
+          AC: Int = 0,
+          Ad: Int = 0,
+          bA: Int = 0,
           AAA: Int = 0,
-          AAa: Int = 0,
-          AaA: Int = 0,
-          aAA: Int = 0,
-          aaA: Int = 0,
-          aAa: Int = 0,
-          Aaa: Int = 0,
-          aaa: Int = 0
+          AAb: Int = 0,
+          AbA: Int = 0,
+          bAA: Int = 0,
+          bbA: Int = 0,
+          bAb: Int = 0,
+          Acc: Int = 0,
+          ccc: Int = 0
       )
       case class SampleFields2(
           aaa123bbb: Int = 0,
@@ -57,34 +48,35 @@ class SnakeCaseJsonSupportSpec extends Specification with SnakeCaseJsonSupport {
       val sampleFields2 = SampleFields2().toJson.asJsObject
       // Then
       val fields1 = sampleFields1.fields
-      fields1.contains(snakify("HelloWorld")) mustEqual true
-      fields1.contains(snakify("A1AAbBBBBBbCCcc")) mustEqual true
-      fields1.contains(snakify("AbAAbbAAAbbb1AA2bb1AA2AAAAbb1bAA")) mustEqual true
-      fields1.contains(snakify("x")) mustEqual true
-      fields1.contains(snakify("Y")) mustEqual true
-      fields1.contains(snakify("^There-arE_Non_(Alphabetic))_character!S")) mustEqual true
-      fields1.contains(snakify("tHere-Are_(AlsO)_noN_(alphaBetic))_CharaCter!s")) mustEqual true
-      fields1.contains(snakify("  There are space character ")) mustEqual true
-      fields1.contains(snakify(" これは　マルチバイトAbcのaBc確認です。")) mustEqual true
-      fields1.contains(snakify("aa")) mustEqual true
-      fields1.contains(snakify("aA")) mustEqual true
-      fields1.contains(snakify("Aa")) mustEqual true
-      fields1.contains(snakify("AA")) mustEqual true
-      fields1.contains(snakify("AAA")) mustEqual true
-      fields1.contains(snakify("aaA")) mustEqual true
-      fields1.contains(snakify("aAa")) mustEqual true
-      fields1.contains(snakify("Aaa")) mustEqual true
-      fields1.contains(snakify("AAa")) mustEqual true
-      fields1.contains(snakify("AaA")) mustEqual true
-      fields1.contains(snakify("aAA")) mustEqual true
-      fields1.contains(snakify("aaa")) mustEqual true
+      fields1 must haveKey ("hello_world")
+      fields1 must haveKey ("a1_a_ab_bbbb_bb_c_ccc")
+      fields1 must haveKey ("ab_a_abb_aa_abbb1_aa2bb1_aa2_aaa_abb1b_aa")
+      fields1 must haveKey ("x")
+      fields1 must haveKey ("y")
+      fields1 must haveKey ("^there-ar_e_non_(alphabetic))_character!s")
+      fields1 must haveKey ("t_here-are_(als_o)_no_n_(alpha_betic))_chara_cter!s")
+      fields1 must haveKey ("  there are space character ")
+      fields1 must haveKey (" これは　マルチバイトabcのa_bc確認です。")
+      fields1 must haveKey ("ab")
+      fields1 must haveKey ("ac")
+      fields1 must haveKey ("ad")
+      fields1 must haveKey ("b_a")
+      fields1 must haveKey ("aaa")
+      fields1 must haveKey ("a_ab")
+      fields1 must haveKey ("ab_a")
+      fields1 must haveKey ("b_aa")
+      fields1 must haveKey ("bb_a")
+      fields1 must haveKey ("b_ab")
+      fields1 must haveKey ("acc")
+      fields1 must haveKey ("ccc")
       val fields2 = sampleFields2.fields
-      fields2.contains(snakify("aaa123bbb")) mustEqual true
-      fields2.contains(snakify("aaa123Bbb")) mustEqual true
-      fields2.contains(snakify("aaA123bbb")) mustEqual true
-      fields2.contains(snakify("aaa123")) mustEqual true
-      fields2.contains(snakify("aaA123")) mustEqual true
-      fields2.contains(snakify("aAA123")) mustEqual true
+      fields2 must haveKey("aaa123bbb")
+      fields2 must haveKey("aa_a123bbb")
+      fields2 must haveKey("aaa123_bbb")
+      fields2 must haveKey("aa_a123_bbb")
+      fields2 must haveKey("aaa123")
+      fields2 must haveKey("aa_a123")
+      fields2 must haveKey("a_aa123")
     }
     "deserialize snake cased json" in {
       // Given
